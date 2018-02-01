8340c62120085e5393f23646c81c75b505e4263c
apple <apple@s-164-67-210-156.resnet.ucla.edu>
2018-02-01 00:30:22 -0800

    file request added

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+var multer  = require('multer')
+var upload = multer({ dest: 'upload/' })
+//var translate = require('../translate');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app =express();
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+
+
+app.post('/translate', upload.single('image'), function (req, res, next) {
+  // req.files is array of `photos` files 
+  console.log(req.file);;
+  // req.body will contain the text fields, if there were any 
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
