6f2e23c5f1ed40fd6a40441476834694ab1f9cbd
apple <apple@s-164-67-210-251.resnet.ucla.edu>
2018-02-02 11:03:43 -0800

    pipeline built

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 var multer  = require('multer')
 var upload = multer({ dest: 'upload/' })
-//var translate = require('../translate');
-const PORT = process.env.PORT || 5000;
+const vision = require('@google-cloud/vision');
+const kuroshiro = require("kuroshiro");
 
+
+// Creates a Google Vision API client
+const client = new vision.ImageAnnotatorClient(); // for OCR
+const PORT = process.env.PORT || 5000;
 var app =express();
 
 
@@ -16,7 +20,26 @@ app.get('/', (req, res) => res.render('pages/index'));
 
 app.post('/translate', upload.single('image'), function (req, res, next) {
   // req.files is array of `photos` files 
-  console.log(req.file);;
+  	var fileName = req.file['path'];
+  	//console.log(fileName);
+	client
+	  .textDetection(fileName)
+	  .then(results => {
+	    const detections = results[0].textAnnotations;
+	    var text = detections[0]['description'];
+	    // console.log(text);
+	    kuroshiro.init(function (err) {
+		    // kuroshiro is ready
+		    var result = kuroshiro.toHiragana(text);    
+		    console.log(result);
+		    res.status(200).send(result);
+		});
+	    // console.log(result);
+	  })
+	  .catch(err => {
+	    console.error('ERROR:', err);
+	  });
+  	
   // req.body will contain the text fields, if there were any 
 });
 
