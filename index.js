b3e307b485e3793ba0f4ff72ab798bc16e1f531a
CSHubbard <CodySpHubbard@gmail.com>
2018-02-10 19:31:16 -0800

    What: Added input validation to the image upload Why: Improve fault handling Who: Cody Hubbard

--- index.js
+++ index.js
@@ -1,57 +1,111 @@
 const express = require('express');
 const path = require('path');
-var multer  = require('multer')
-var upload = multer({ dest: 'upload/' })
 const vision = require('@google-cloud/vision');
 const kuroshiro = require("kuroshiro");
+var multer  = require('multer');
+var expressValidator = require('express-validator')
 
+// Creates Express app
+var app = express();
+
+// view engine setup
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Creates a Google Vision API client
 const client = new vision.ImageAnnotatorClient(); // for OCR
 const PORT = process.env.PORT || 5000;
-var app = express();
 
+// Multer file upload 
+var upload = multer({ 
+	dest: 'upload/',
+    limits: { 				//limits is for file verification
+      fieldNameSize: 255,
+      fileSize: 5242880,
+      files: 1,
+      fields: 1
+    }})
+
+	//express validator
+app.use(function(req, res, next){
+	res.locals.errors = null;
+	next();
+});
+
+app.use(expressValidator({
+  customValidators: {
+    isImage: function(value, mimetype) {
+		if (mimetype == undefined)
+			return false;
+		switch (mimetype) {
+			case 'image/jpg':
+				return true;
+			case 'image/jpeg':
+                return true;
+            case  'image/png':
+                return true;
+            default:
+                return false;
+}}}}));
+	
+// Object to hold all image data
 var UserImage = {
 	filename:"placeholder text",
 	textDetections:"placeholder text",
-	textTranslation:"placeholder text"
+	textPronunciation:"placeholder text"
 }	//this is an object to hold translation data
 	//the repeat statements below are to be there 
 	//until i can make sure nothing breaks by using it only
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index', {
 		UserImage: UserImage
 	}));
 
 app.post('/translate', upload.single('image'), function (req, res, next) {
-  // req.files is array of `photos` files 
+	// Image validation
+	req.checkBody('image', 'Please upload an image file').isImage(req.file.mimetype);
+	
+	var errors = req.validationErrors();
+	console.log(req.validationErrors());
+	if(errors){
+		res.render('pages/index', {
+		UserImage: UserImage,
+		errors: errors})
+	} else {
+		
+	// req.files is array of `photos` files 
   	var fileName = req.file['path'];
 	UserImage.fileName = fileName;
-  	//console.log(fileName);
+
 	client
 	  .textDetection(fileName)
 	  .then(results => {
 	    const detections = results[0].textAnnotations;
 	    var text = detections[0]['description'];
 		UserImage.textDetections = text;
-	    // console.log(text);
+		
+		// kuroshiro is ready
 	    kuroshiro.init(function (err) {
-		    // kuroshiro is ready
 		    var result = kuroshiro.toHiragana(text);
-			UserImage.textTranslation = result;		
-		    //console.log(result);
-		    //res.status(200).send(result);
+			UserImage.textPronunciation = result;		
 		});
-	    // console.log(result);
 	  })
-	  .catch(err => {
-	    console.error('ERROR:', err);
-	  });
-  	
+	  .catch(next);
+
+  	res.redirect('/');
   // req.body will contain the text fields, if there were any 
+	}
 });
+
+//Error Handeling
+app.use(function(err, req, res, next){
+	res.status(400).render('pages/index', {
+		UserImage: UserImage,
+		errors: [{msg: err.message}]
+	});
+	console.log(errors);
+	//res.status(400).send({error: err.message});
+});	
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
